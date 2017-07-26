01d173c6469558bac2834a7b242aa4c6f70260b5
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2017-07-26 14:33:40 +0500

    Finalized the pwa app with appshell, service worker and push notifications

--- run.sh
+++ run.sh
@@ -5,6 +5,15 @@ set -x
 # Production build
 ng build --prod
 
+# generate new index.html using app shell
+
+node_modules/.bin/ngu-app-shell --module src/app/app.module.ts \
+    --out dist/index.html
+
+# merge ngsw-manifest and copy to dist
+./node_modules/.bin/ngu-sw-manifest --module src/app/app.module.ts \
+                --out dist/ngsw-manifest.json
+
 # Serve
 cd dist
 http-server

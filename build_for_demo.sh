c3c06ddda0ab364e584a6aea03c4de39eb10a904
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2017-07-27 00:42:07 +0500

    Providing complete base-href for demo

--- build_for_demo.sh
+++ build_for_demo.sh
@@ -8,10 +8,10 @@ ng build -bh "https://ahsanayaz.github.io/ng-books-pwa/" --prod
 # generate new index.html using app shell
 
 node_modules/.bin/ngu-app-shell --module src/app/app.module.ts \
-    --base-href "/ng-books-pwa/" \
+    --base-href "https://ahsanayaz.github.io/ng-books-pwa/" \
     --out dist/index.html
 
 # merge ngsw-manifest and copy to dist
 ./node_modules/.bin/ngu-sw-manifest --module src/app/app.module.ts \
-                --base-href "/ng-books-pwa/" \
+                --base-href "https://ahsanayaz.github.io/ng-books-pwa/" \
                 --out dist/ngsw-manifest.json
